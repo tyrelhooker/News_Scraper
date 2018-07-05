4a7f872b2fd8e6f5ad76dca8780820c9be78da72
T.J. Hooker <tyrelhooker@gmail.com>
2018-07-05 00:57:12 -0400

    changed server.js port info

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var exphbs = require("express-handlebars");
 var cheerio = require("cheerio");
 var request = require("request");
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
 var app = express();
 
 //Set Handlebars
